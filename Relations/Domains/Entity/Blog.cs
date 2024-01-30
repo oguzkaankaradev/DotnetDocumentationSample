5710036b3a3250770896063e4831085d838fc969
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-01-30 09:54:47 +0800

    ForeginKey

---
+++ Relations/Domains/Entity/Blog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.Entity
+{
+    public class Blog
+    {
+        public BlogHeader? Header { get; set; }  // Reference navigation to dependent
+    }
+}
