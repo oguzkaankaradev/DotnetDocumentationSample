9ac0a45935b85157616188cfc827605fc63fe7e3
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-01-31 15:18:28 +0800

    SystemTextEncodingWeb

--- MVC/MVC/Controllers/HelloWorldController.cs
+++ MVC/MVC/Controllers/HelloWorldController.cs
@@ -1,18 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace MVC.Controllers
 {
     public class HelloWorldController : Controller
     {
-
+        //https://localhost:5001/HelloWorld.
+        // GET: /HelloWorld/
         public string Index()
         {
             return "This is my default project...";
         }
 
+        // GET: /HelloWorld/Welcome/ 
+
         public string Welcome()
         {
             return "This is the welcome action method...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        //Run the app and browse to: https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4. Replace {PORT} with your port number.
+
+
+        public string SystemTextEncodingWeb(string name, int numTimes =1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is : {numTimes}");
         }
     }
 }
--- MVC/MVC/Program.cs
+++ MVC/MVC/Program.cs
@@ -20,6 +20,10 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+/*MVC invokes controller classes, and the action methods within 
+ * them, depending on the incoming URL. The default 
+ * URL routing logic used by MVC, 
+ * uses a format like this to determine what code to invoke:*****************************/
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
