70e5127ef59f4c3bd61da42517f03499eb1844c2
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-01-31 15:09:30 +0800

    HelloWorldController.cs

---
+++ MVC/MVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public string Index()
+        {
+            return "This is my default project...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
