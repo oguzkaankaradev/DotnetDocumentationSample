4dd051ffa38124f503a66f458bef1056f28cdee0
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-02-01 10:04:46 +0800

    MoviesController

--- MVC/MVC/Program.cs
+++ MVC/MVC/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MVCContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCContext") ?? throw new InvalidOperationException("Connection string 'MVCContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
