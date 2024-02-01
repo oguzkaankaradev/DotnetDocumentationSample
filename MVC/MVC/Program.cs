9c06d48e0f71ced0f728235f73f95e902ade43ce
OĞUZHAN KARA <oguz.kaan.kara@outlook.com>
2024-02-01 13:00:44 +0800

    AddedSeedData

--- MVC/MVC/Program.cs
+++ MVC/MVC/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<MVCContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MVCContext") ?? throw new InvalidOperationException("Connection string 'MVCContext' not found.")));
 
